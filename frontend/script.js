690720275717dc2c377db67f8adce125137c8310
matka00 <anita.skumat@gmail.com>
2022-04-01 12:48:11 +0200

    Friday's mentoring

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+console.log("ez az első javascriptem")
+/*
+
+// a stringeket (=szövegeket) idézőjelbe kell rakni
+"ez itt egy szöveg"
+
+// a dupla és sima idézőjel csak egysoros stringet (=szöveget) enged meg
+'ez itt másik szöveg'
+
+// backtick-ek közé rakott stringben lehet sortörés
+`ez egy több
+
+soros string (szöveg)`
+
+// a számokat nem kell idézőjelbe rakni
+4
+128
+04
+1.13
+*/
+
+function myFirstFunction(){
+    console.log("ez az első függvényem belsejében lévő log")
+}
+
+myFirstFunction()
